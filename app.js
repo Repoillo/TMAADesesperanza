34f4c8fd7666cab6d9dec6bb22a129bb37f9aace
Repo <alextin2609@gmail.com>
2025-10-06 11:40:58 -0600

    actualizacion 0.01

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const express = require('express');
-const mysql = require('mysql2/promise'); 
+const mysql = require('mysql2'); 
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.static('public'));
 
-// uso .env para poder subrilo a github
+// usamos .env para no subir datos sensibles como contraseñas a GitHub
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME
+});
+
+// Este bloque prueba la conexión al iniciar el servidor
+db.connect(err => {
+  if (err) {
+    // si el parametro 'err' llega con algo, es que hubo un error
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  // si 'err' llega vacio, todo salio bien
+  console.log('Conexión exitosa a la base de datos');
 });
 
 // === Rutas ===
 
+
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
